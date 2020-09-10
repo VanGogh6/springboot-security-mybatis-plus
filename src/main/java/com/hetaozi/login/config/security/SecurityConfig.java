3565e6e399e41943a983950ea3b8db149e25d713
van <520200wf@163.com>
2020-09-10 23:42:35 +0800

    存在整合security和swagger时，security拦截了swagger登陆

---
+++ src/main/java/com/hetaozi/login/config/security/SecurityConfig.java
@@ -0,0 +1,12 @@
+package com.hetaozi.login.config.security;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 吴帆
+ * @date 2020/9/10 0010 23:39
+ */
+@Configuration
+public class SecurityConfig {
+
+}
